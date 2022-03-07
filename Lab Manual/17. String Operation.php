0602516e871d2bbc06399d088857321e8a5a7760
jay <jaysparmar303@gmail.com>
2022-03-08 00:53:16 +0530

    update gitIgnore & add some OP & some minimal chnages

--- Lab Manual/17. String Operation.php
+++ Lab Manual/17. String Operation.php
@@ -1,4 +1,27 @@
 <!-- 17 Write a program to do String Operations PHP.  -->
 <?php
+	
+	$str1 = "Hello world";
+	$str2 = "world";
+
+	echo "str1 = " . $str1 . "<br>";
+	echo "str2 = " . $str2 . "<br>";
+	echo "String length str1 : " . strlen($str1) . "<br>";
+	echo "strstr(str1, str2) : " . strstr($str1, $str2) . "<br>";
+	echo "strpos(str1, str2) : " . strpos($str1, $str2) . "<br>";
+	echo "trim(' H   E   L    L   O ') : '" . trim(' H   E   L    L   O ') . "'<br>";
+	echo "strtoupper(str1) : " . strtoupper($str1) . "<br>";
+	echo "strrev(str1) : " . strrev($str1) . "<br>";
 
 ?>
+<!-- 
+O/P :-
+str1 = Hello world
+str2 = world
+String length str1 : 11
+strstr(str1, str2) : world
+strpos(str1, str2) : 6
+trim(' H E L L O ') : 'H E L L O'
+strtoupper(str1) : HELLO WORLD
+strrev(str1) : dlrow olleH
+ -->
---
+++ Lab Manual/20. Session.php
@@ -0,0 +1,9 @@
+<!-- 
+20. Write a PHP program to send value using session across the 
+pages.
+-->
+<?php
+
+
+
+?>
